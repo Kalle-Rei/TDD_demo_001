9a0e4f529a7fc44f599674b5ab5b8a10e1bf9608
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-21 13:11:20 +0100

    refactored add() to work with any number of numbers

--- src/main/java/se/kalle/Calculator.java
+++ src/main/java/se/kalle/Calculator.java
@@ -5,14 +5,9 @@ public class Calculator {
     public static int add(final String numbers){
         int returnValue = 0;
         String[] numbersArray = numbers.split(",");
-        if (numbersArray.length > 2){
-            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
-        }
-        else{
-            for (String number : numbersArray){
-                if (!number.trim().isEmpty()){
-                    returnValue += Integer.parseInt(number); // if it is not a number parseInt will throw an exception
-                }
+        for (String number : numbersArray) {
+            if (!number.trim().isEmpty()) {
+                returnValue += Integer.parseInt(number); // if it is not a number parseInt will throw an exception
             }
         }
         return returnValue;
