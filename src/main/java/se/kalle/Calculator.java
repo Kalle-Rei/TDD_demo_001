824f2de62d8569b984045ef5b3451a80f74e78d8
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-21 13:04:15 +0100

    refactored add() to work with the new tests

--- src/main/java/se/kalle/Calculator.java
+++ src/main/java/se/kalle/Calculator.java
@@ -2,18 +2,19 @@ package se.kalle;
 
 public class Calculator {
 
-    public static final int add(final String numbers){
+    public static int add(final String numbers){
+        int returnValue = 0;
         String[] numbersArray = numbers.split(",");
         if (numbersArray.length > 2){
             throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
         }
         else{
             for (String number : numbersArray){
-                if (!number.isEmpty()){
-                    Integer.parseInt(number); // if it is not a number parseInt will throw an exception
+                if (!number.trim().isEmpty()){
+                    returnValue += Integer.parseInt(number); // if it is not a number parseInt will throw an exception
                 }
             }
         }
-        return 0;
+        return returnValue;
     }
 }
