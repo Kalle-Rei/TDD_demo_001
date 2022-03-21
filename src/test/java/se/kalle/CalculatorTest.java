aa27cf05391bc6c0f9cbcffedf2660ac33423188
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-21 11:21:26 +0100

    implemented add()

--- src/test/java/se/kalle/CalculatorTest.java
+++ src/test/java/se/kalle/CalculatorTest.java
@@ -5,8 +5,17 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+    @Test(expected = RuntimeException.class)
+    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown(){
+        Calculator.add("1,2,3");
+    }
     @Test
-    void willNotPass(){
-        Assert.assertTrue("this test will fail", false);
+    public final void when2NumbersAreUsedThenNoExceptionIsThrown(){
+        Calculator.add("1,2");
+        Assert.assertTrue(true);
+    }
+    @Test(expected = RuntimeException.class)
+    public final void whenNonNumberIsUsedThenExceptionIsThrown(){
+        Calculator.add("1,X");
     }
 }
