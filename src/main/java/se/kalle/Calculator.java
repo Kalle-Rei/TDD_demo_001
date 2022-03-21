f7b9eddebee632773d1eb4842072871db9b0c8d2
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-21 13:16:07 +0100

    made add() handle line breaks

--- src/main/java/se/kalle/Calculator.java
+++ src/main/java/se/kalle/Calculator.java
@@ -4,10 +4,10 @@ public class Calculator {
 
     public static int add(final String numbers){
         int returnValue = 0;
-        String[] numbersArray = numbers.split(",");
+        String[] numbersArray = numbers.split(",|n");
         for (String number : numbersArray) {
             if (!number.trim().isEmpty()) {
-                returnValue += Integer.parseInt(number); // if it is not a number parseInt will throw an exception
+                returnValue += Integer.parseInt(number.trim());
             }
         }
         return returnValue;
