13064a6e9e18f66cab2bc208961edcf60f2ef745
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-21 13:31:20 +0100

    added tests for negative numbers

--- src/test/java/se/kalle/CalculatorTest.java
+++ src/test/java/se/kalle/CalculatorTest.java
@@ -5,6 +5,22 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+    @Test(expected = RuntimeException.class)
+    public final void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown(){
+        Calculator.add("3,-6,15,18,46,33");
+    }
+    @Test
+    public final void whenNegativeNumbersAreUsedThenRuntimeExceptionIsThrown(){
+        RuntimeException exception = null;
+        try {
+            Calculator.add("3,-6,15,18,46,33");
+        } catch (RuntimeException e){
+            exception = e;
+        }
+        Assert.assertNotNull(exception);
+        Assert.assertEquals("Negatives are not allowed: [-6, -18]", exception.getMessage());
+    }
+
     @Test
     public final void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums(){
         Assert.assertEquals(3+6+15, Calculator.add("3,6n15"));
