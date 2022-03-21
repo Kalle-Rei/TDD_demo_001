34354eb82ed7b0f16d8401ff0c483f889d887d36
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-21 13:13:55 +0100

    tests for an add() that can handle line breaks

--- src/test/java/se/kalle/CalculatorTest.java
+++ src/test/java/se/kalle/CalculatorTest.java
@@ -5,10 +5,11 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-//    @Test(expected = RuntimeException.class)
-//    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown(){
-//        Calculator.add("1,2,3");
-//    }
+    @Test
+    public final void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums(){
+        Assert.assertEquals(3+6+15, Calculator.add("3,6n15"));
+    }
+
     @Test
     public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesIsTheirSums(){
         Assert.assertEquals(3+6+15+18+46+33, Calculator.add("3,6,15,18,46,33"));
